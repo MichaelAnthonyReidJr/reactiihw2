d8e386b46a694be5a0aa3ffd5c2e24e0cb95c9fe
Michael Reid <mreid@xpanxion.com>
2023-05-23 21:41:45 -0400

    Initial commit

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Square = () => {
+    return (
+        <div style ={{width: '710px', height: '710px', background: 'linear-gradient(to bottom right,#f800ff, #260239)', 
+                        display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                            Hello World!
+        
+        </div>
+    )
+}
